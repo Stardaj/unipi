026a0ec9c5bc58635442cd4217eecb4eea056002
gfranchini96 <61594836+gfranchini96@users.noreply.github.com>
2020-12-13 00:34:32 +0100

    keyboard accessibility (2/2)
    fatto uno sbocco per rendere navigabile con la tastiera le storie di Platone. Due problemi: uno poco rilevante, ovvero che dalla fine della storia anziché tornare al pulsante di chiusura del div in cui è contenuta la storia va al pulsante dopo (a meno di non usare un trucchetto che però è giusto un tappabuchi poco razionale). Secondo problema: forse sarebbe meglio (non davvero ma dire che sarebbe stato meglio) paginare le immagini (tipo 10 alla volta) e dare all'utente che usa solo la tastiera la possibilità di uscire dalla storia prima di averla finita con un pulsante alternativo al caricamento di ulteriori immagini. Utile anche per evitare tempi di caricamento biblici (perché le immagini non sarebbero caricate tutte in una volta)

--- SitoTAD/js/scripts.js
+++ SitoTAD/js/scripts.js
@@ -29,6 +29,30 @@
             }
         }
     });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").clickollapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 100,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
     $('[class="sym-img"]').each(function(){
         $(this).click(function(){
             if($(this).parent().parent().attr('id') == "mitoitaliano") {
@@ -44,7 +68,7 @@
                 var traduzione = $(this).attr("alt");
                 $("#modal_greco > div").text(traduzione);
             }
-        })
+        });
         $(this).keyup(function(event){
             if(event.which == "13") {
                 $('a, button, img[tabindex="0"]').not('[tabindex="-1"]').each(function() {
@@ -68,13 +92,13 @@
                     $("[id='close_greco']").focus();
                 }
             }
-        })
-    })
+        });
+    });
 
     $("[class='close']").each(function(){
         $(this).click(function(){
             $(this).parent().css("display","none");
-        })
+        });
         $(this).keyup(function(event){
             if(event.which == "13") {
                 $('[tabindex=-2]').each(function() {
@@ -85,30 +109,74 @@
                     })
                 $(this).parent().css("display","none");
             }
-        })
+        });
+    });
+    // inizio sezione navigazione da tastiera
+    // un difetto che mi viene in mente: se qualcuno naviga sia da tastiera, sia
+    // da mouse, non funziona più l'autofocus sulla prima immagine (perché non ho scritto
+    // l'apposito codice per il click)
+    // altro problema: non ho trovato il modo di assorbire il keyup dopo il keydown, quindi 
+    // a volte la tab viene eseguita (non capisco il criterio, comunque succede solo con le immagini. Meglio di niente)
+    var stato_italiano = false;
+    var stato_greco = false;
+    $('[href="#autori"]').keyup(function(event){
+        if(event.which == "13") {
+            $("#autori").find("a").not('[tabindex="-1"]').first().focus();
+        }
+    });
+    $("#autori").find("a").not('[tabindex="-1"]').last().keydown(function(event){
+        if(event.which == "9") {
+            $('[href="#contact_section"]').focus();
+        }
+    });
+    $('[href="#contact_section"]').keyup(function(event){
+        if(event.which == "13") {
+            $("#contact_section").find("a").not('[tabindex="-1"]').first().focus();
+        }
+    });
+    $('[href="#platone"] > button').keyup(function(event){
+        if(event.which == "13") {
+            $("#platone").find("a").not('[tabindex="-1"]').first().focus();
+        }
+    });
+    $("#platone").find("a").not('[tabindex="-1"]').last().keydown(function(event) {
+        if(event.which == "9") {
+            $('[href="#autori"]').focus();
+        }
+    });
+    $('[href="#mitoitaliano"]').keyup(function(event){
+        if(event.which == "13") {
+            console.log("ok invio");
+            if(!stato_italiano) {
+                console.log("ok falso");
+                stato_italiano = true;
+                stato_greco = false;
+                $("#mitoitaliano").find('img[tabindex="0"]').first().focus();
+            } else {
+                stato_italiano = false;
+            }
+        }
+    });
+    $("#mitoitaliano").find('img[tabindex="0"]').last().keydown(function(event){
+        if(event.which == "9") {
+            $('[href="#mitoitaliano"]').focus();
+        }
+    });
+    $('[href="#mitogreco"]').keyup(function(event){
+        if(event.which == "13") {
+            if(!stato_greco) {
+                stato_greco = true;
+                stato_italiano = false;
+                $("#mitogreco").find('img[tabindex="0"]').first().focus();
+            } else {
+                stato_greco = false;
+            }
+        }
+    });
+    $("#mitogreco").find('img[tabindex="0"]').last().keydown(function(event){
+        if(event.which == "9") {
+            $('[href="#mitoitaliano"]').focus(); // qua ho messo #mitoitaliano anziché #greco perché così va nel posto giusto (cioè su #mitogreco)
+        }
     })
 
-  // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
